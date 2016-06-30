5e1f92eb045cf9582529b6b05576cf2d8e4af73d
Nicolas Barral <n.barral@imperial.ac.uk>
2016-06-30 17:11:16 +0100

    Solver improvement: only one Var. For.

--- solve.py
+++ solve.py
@@ -22,14 +22,15 @@ def computeDtAdvecOld(meshd, cn, cxExpr, cyExpr, options) :
     
 def computeDtAdvec(meshd, cn, cxExpr, cyExpr, options) :
     
-    nrmc = "sqrt((%s)*(%s)+(%s)*(%s))" % (cxExpr, cxExpr, cyExpr, cyExpr)
-    cn.interpolate(Expression(nrmc))
-    
-    dt = 1e10
-    for iVer in range(meshd.mesh.topology.num_vertices()):
-        dtloc = meshd.altMin.dat.data[iVer] / (cn.dat.data[iVer]+1e-10)
-        dt = min(dt, dtloc)    
+#    dt = 1e10
+#    for iVer in range(meshd.mesh.topology.num_vertices()):
+#        dtloc = meshd.altMin.dat.data[iVer] / (cn.dat.data[iVer]+1e-10)
+#        dt = min(dt, dtloc)   
+
+    dt = (meshd.altMin.dat.data / (cn.dat.data+1e-10)).min()
     dt *= options.cfl
+
+
     
     return dt
     
@@ -93,15 +94,35 @@ def solveAdvec(meshd, solIni, tIni, tEnd, options):
     
     Q = FunctionSpace(mesh, "CG", 1)
     V = VectorFunctionSpace(mesh, "CG", 2)
-    Vn = FunctionSpace(mesh, "CG", 2)
+    Vn = FunctionSpace(mesh, "CG", 1)
     M = TensorFunctionSpace(mesh, 'CG', 1)
     u, v = TrialFunction(Q), TestFunction(Q)
     u0 = Function(Q)     
-    c, cn = Function(V), Function(Vn) # velocity and its norm
+    c0, cn = Function(V), Function(Vn) # velocity base and its norm
     hessian = Function(M)
+
+
             
     zero = Constant(0)
     bc = DirichletBC(Q, zero, 1)
+
+    time = Constant(0)
+    delta_t = Constant(0.1)
+    cxExpr = "-sin(pi*x[0])*sin(pi*x[0])*sin(2*pi*x[1])" 
+    cyExpr = "sin(2*pi*x[0])*sin(pi*x[1])*sin(pi*x[1])"
+    c0.interpolate(Expression([cxExpr, cyExpr]))
+    c = c0*cos(2*pi*time/T)
+
+    a = v*u*dx + 0.5*delta_t*(v*dot(c, grad(u))*dx)
+    L = v*u0*dx - 0.5*delta_t*(v*dot(c, grad(u0))*dx)
+    # Add SUPG stabilization terms
+    ra = u + 0.5*delta_t*(dot(c, grad(u)))
+    rL = u0 - 0.5*delta_t*(dot(c, grad(u0)))
+    cnorm = sqrt(dot(c, c))
+    h = meshd.altMin
+    a += (h/(2.*cnorm))*dot(c, grad(v))*ra*dx
+    L += (h/(2.*cnorm))*dot(c, grad(v))*rL*dx
+
 
     nbrSpl = options.nbrSpl 
     dtSpl = float(tEnd-tIni)/(nbrSpl-1)
@@ -130,9 +151,10 @@ def solveAdvec(meshd, solIni, tIni, tEnd, options):
         step += 1
         print "####  step %d " % step ; sys.stdout.flush()
     
-        cxExpr = "-sin(pi*x[0])*sin(pi*x[0])*sin(2*pi*x[1])*cos(2*pi*%f)" % (t/T)
-        cyExpr = "sin(2*pi*x[0])*sin(pi*x[1])*sin(pi*x[1])*cos(2*pi*%f)" % (t/T)
-        c.interpolate(Expression([cxExpr, cyExpr]))
+        time.assign(t)
+#        c.interpolate(c0*cos(2*pi*t/T))
+#        cnorm = sqrt(dot(c, c))
+        cn.interpolate(cnorm)
     
         dt = computeDtAdvec(meshd, cn, cxExpr, cyExpr, options)
         if (options.nbrSav > 0) and (t < tIni+(stepSav+1)*dtSav) and (t+dt >= tIni+(stepSav+1)*dtSav) :
@@ -148,6 +170,7 @@ def solveAdvec(meshd, solIni, tIni, tEnd, options):
             print "DEBUG  Trunc dt to final time"
             endSol = 1
             dt = tEnd - t + + 1.e-5*dt
+        delta_t.assign(dt)
         doSav = 0
         if (options.nbrSav > 0) and ((t < tIni+(stepSav+1)*dtSav) and (t+dt >= tIni+(stepSav+1)*dtSav) or endSol):
             stepSav += 1
@@ -156,19 +179,19 @@ def solveAdvec(meshd, solIni, tIni, tEnd, options):
         if ((t < tIni+(stepSpl+1)*dtSpl) and (t+dt >= tIni+(stepSpl+1)*dtSpl) or endSol) :
             stepSpl += 1
             doSpl = 1
-
         
         print "DEBUG  t:  %1.3e -> %1.3e with dt: %1.7e" %(t, t+dt, dt)
-        a = v*u*dx + 0.5*dt*(v*dot(c, grad(u))*dx)
-        L = v*u0*dx - 0.5*dt*(v*dot(c, grad(u0))*dx)
-        # Add SUPG stabilization terms
-        ra = u + 0.5*dt*(dot(c, grad(u)))
-        rL = u0 - 0.5*dt*(dot(c, grad(u0)))
-        cnorm = sqrt(dot(c, c))
-        h = meshd.altMin
-        a += (h/(2.*cnorm))*dot(c, grad(v))*ra*dx
-        L += (h/(2.*cnorm))*dot(c, grad(v))*rL*dx
-        solve(a==L,u0)
+        
+#        a = v*u*dx + 0.5*delta_t*(v*dot(c, grad(u))*dx)
+#        L = v*u0*dx - 0.5*delta_t*(v*dot(c, grad(u0))*dx)
+#        # Add SUPG stabilization terms
+#        ra = u + 0.5*delta_t*(dot(c, grad(u)))
+#        rL = u0 - 0.5*delta_t*(dot(c, grad(u0)))
+##        cnorm = sqrt(dot(c, c))
+#        h = meshd.altMin
+#        a += (h/(2.*cnorm))*dot(c, grad(v))*ra*dx
+#        L += (h/(2.*cnorm))*dot(c, grad(v))*rL*dx
+        solve(a==L, u0, bcs=bc)
         
         t += dt
         
